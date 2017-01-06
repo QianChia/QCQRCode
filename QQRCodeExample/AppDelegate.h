d11bfb6d5c0b0baf6b6b02a9f2528353bac1a988
仟 <qianchia@icloud.com>
2017-01-06 14:14:32 +0800

    Initial commit

---
+++ QQRCodeExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQRCodeExample
+//
+//  Created by JHQ0228 on 2017/1/5.
+//  Copyright © 2017年 QianQian-Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # QCQRCode
+
+- 二维码扫描 Demo
+
+- 开发环境：Xcode 8.2
+
+- 二维码扫描说明见 [QianChia 的博客](http://www.cnblogs.com/QianChia/p/6213466.html)。
